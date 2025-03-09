3140309ee73f1d5e57fd5e1320c572dc79606351
theApsil <goncharuk.dm@students.dvfu.ru>
2025-03-10 01:59:38 +1000

    UPD: New buildings types

--- crud.py
+++ crud.py
@@ -1,6 +1,19 @@
 from config import db
 from models import TypeBuilding
 
-item = TypeBuilding('Небоскрёб')
-db.session.add(item)
-db.session.commit()
+BUILDINGS = [
+     'Антенная мачта', 
+     'Бетонная башня', 
+     'Радиомачта', 
+     'Гиперболоидная башня', 
+     'Дымовая труба', 
+     'Решётчатая мачта', 
+     'Башня', 
+     'Мост'
+]
+
+if __name__ == "__main__":
+    for name in BUILDINGS:
+        item = TypeBuilding(name)
+        db.session.add(item)
+    db.session.commit()
